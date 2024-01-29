78ef91fb9c6d5f8348d4df746123ae45dbf5291d
Hmoo Myat Theingi <theingihmoomyat@gmail.com>
2024-01-29 11:04:54 +0630

    Add read more button

--- Aspen Travel App/PopularDetailViewController.swift
+++ Aspen Travel App/PopularDetailViewController.swift
@@ -15,12 +15,26 @@ class PopularDetailViewController: UIViewController {
     @IBOutlet var backView: UIView!
     @IBOutlet var popularPlaceImage: UIImageView!
     @IBOutlet var wishlistView: UIView!
-
+    @IBOutlet var popularPlaceOverview: UILabel!
     @IBOutlet var popularPlaceTitle: UILabel!
-    
     @IBOutlet var bookNowButtonView: UIView!
+    @IBOutlet var readMoreButton: UIButton!
+    
+    @IBOutlet var facilitiesCollectionView: UICollectionView!
+    
+    let maxLines: Int = 3
+    var isExpanded: Bool = false
+      let seeMoreButton: UIButton = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
+                popularPlaceOverview.text = "Aspen is as close as one can get to a storybook alpine town in America. The choose-your-own-adventure possibilities—skiing, hiking, dining shopping..."
+
+
+                if let image = selectedImage, let title = selectedTitle {
+                    popularPlaceImage.image = image
+                    popularPlaceTitle.text = title
+                }
+
         
         if let image = selectedImage,
            let title = selectedTitle
@@ -42,11 +56,37 @@ class PopularDetailViewController: UIViewController {
         wishlistView.layer.masksToBounds = false
         bookNowButtonView.layer.cornerRadius = 10
         bookNowButtonView.clipsToBounds = true
+//        facilitiesContentView.layer.cornerRadius = 10
+//        facilitiesContentView.clipsToBounds = true
         
     }
     
+    
+    @IBAction func readMoreButtonTapped(_ sender: UIButton) {
+        isExpanded.toggle()
+        updateOverviewText()
+    }
+    func updateOverviewText() {
+        let fullText = "Aspen is as close as one can get to a storybook alpine town in America. The choose-your-own-adventure possibilities—skiing, hiking, dining shopping and Aspen is as close as one can get to a storybook alpine town in America. The choose-your-own-adventure possibilities—skiing, hiking, dining shopping."
+
+        if isExpanded {
+            popularPlaceOverview.text = fullText
+            seeMoreButton.setTitle("See Less", for: .normal)
+        } else {
+            let truncatedText = String(fullText.prefix(148)) + "..."
+            popularPlaceOverview.text = truncatedText
+            seeMoreButton.setTitle("See More", for: .normal)
+        }
+    }
+
+  
     @IBAction func closeButtonTapped(_ sender: UIButton) {
            dismiss(animated: true, completion: nil)
        }
+    
+   
+}
+
+class FacilitiesCollectionViewCell : UICollectionViewCell{
     
 }
